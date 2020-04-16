3bd3c6217f10e405dcf6da429678541f889fabab
albisk96 <albertas.kruzintaitis24@gmail.com>
2020-04-16 19:07:54 +0300

    Workout Generation In Progess

--- helpers/plan-helper.js
+++ helpers/plan-helper.js
@@ -1,7 +1,5 @@
 
 function GeneratePlan(athlete, exercise){
-    let exerciseNumberForBeginner = 3;
-    let exerciseNumberForIntermediate = 4;
     
     let days = athlete.days_per_week;
     let level = athlete.level;
@@ -9,44 +7,65 @@ function GeneratePlan(athlete, exercise){
     let sets = null;
     let reps = null;
     let rest = null;
+    let gender = athlete.gender;
+
     const isolationExercises = {
         chest: exercise.filter(x => x.muscles === 'chest' && x.mechanicsType === 'isolation'),
-        back: exercise.filter(x => (x.muscles === 'middle back' || x.muscles === 'middle back' || x.muscles === 'lats') && x.mechanicsType === 'isolation'),
+        back: exercise.filter(x => (x.muscles === 'middle back' || x.muscles === 'lower back' || x.muscles === 'lats') && x.mechanicsType === 'isolation'),
         shoulders: exercise.filter(x => x.muscles === 'shoulders' && x.mechanicsType === 'isolation'),
-        legs: exercise.filter(x => (x.muscles === 'calves' || x.muscles === 'glutes' || x.muscles === 'legs') && x.mechanicsType === 'isolation') ,
+        legs: exercise.filter(x => (x.muscles === 'calves' || x.muscles === 'legs') && x.mechanicsType === 'isolation') ,
         triceps: exercise.filter(x => x.muscles === 'triceps' && x.mechanicsType === 'isolation'),
         biceps: exercise.filter(x => x.muscles === 'biceps' && x.mechanicsType === 'isolation'),
+        legsForWomen: exercise.filter(x => (x.muscles === 'calves' || x.muscles === 'legs' || x.muscles === 'glutes') && x.mechanicsType === 'isolation'),
     }
-
     const compoundExercises = {
         chest: exercise.filter(x => x.muscles === 'chest' && x.mechanicsType === 'compound'),
-        back: exercise.filter(x => (x.muscles === 'middle back' || x.muscles === 'middle back' || x.muscles === 'lats') && x.mechanicsType === 'compound'),
+        back: exercise.filter(x => (x.muscles === 'middle back' || x.muscles === 'lower back' || x.muscles === 'lats') && x.mechanicsType === 'compound'),
         shoulders: exercise.filter(x => x.muscles === 'shoulders' && x.mechanicsType === 'compound'),
-        legs: exercise.filter(x => (x.muscles === 'calves' || x.muscles === 'glutes' || x.muscles === 'legs') && x.mechanicsType === 'compound') ,
+        legs: exercise.filter(x => (x.muscles === 'calves' || x.muscles === 'legs') && x.mechanicsType === 'compound') ,
         triceps: exercise.filter(x => x.muscles === 'triceps' && x.mechanicsType === 'compound'),
         biceps: exercise.filter(x => x.muscles === 'biceps' && x.mechanicsType === 'compound'),
+    }
+    const isolationExercisesForBeginner = {
+        chest: exercise.filter(x => x.muscles === 'chest' && x.mechanicsType === 'isolation'  && x.level === 'beginner'),
+        back: exercise.filter(x => (x.muscles === 'middle back' || x.muscles === 'lower back' || x.muscles === 'lats') && x.mechanicsType === 'isolation' && x.level === 'beginner'),
+        shoulders: exercise.filter(x => x.muscles === 'shoulders' && x.mechanicsType === 'isolation' && x.level === 'beginner'),
+        legs: exercise.filter(x => (x.muscles === 'calves' || x.muscles === 'legs') && x.mechanicsType === 'isolation' && x.level === 'beginner') ,
+        triceps: exercise.filter(x => x.muscles === 'triceps' && x.mechanicsType === 'isolation'  && x.level === 'beginner'),
+        biceps: exercise.filter(x => x.muscles === 'biceps' && x.mechanicsType === 'isolation' && x.level === 'beginner'),
+        legsForWomen: exercise.filter(x => (x.muscles === 'calves' || x.muscles === 'legs' || x.muscles === 'glutes') && x.mechanicsType === 'isolation'),
+    }
+    const compoundExercisesForBeginner = {
+        chest: exercise.filter(x => x.muscles === 'chest' && x.mechanicsType === 'compound' && x.level === 'beginner'),
+        back: exercise.filter(x => (x.muscles === 'middle back' || x.muscles === 'lower back' || x.muscles === 'lats') && x.mechanicsType === 'compound' && x.level === 'beginner'),
+        shoulders: exercise.filter(x => x.muscles === 'shoulders' && x.mechanicsType === 'compound' && x.level === 'beginner'),
+        legs: exercise.filter(x => (x.muscles === 'calves' || x.muscles === 'legs') && x.mechanicsType === 'compound' && x.level === 'beginner') ,
+        triceps: exercise.filter(x => x.muscles === 'triceps' && x.mechanicsType === 'compound'  && x.level === 'beginner'),
+        biceps: exercise.filter(x => x.muscles === 'biceps' && x.mechanicsType === 'compound' && x.level === 'beginner'),
     }
 
     const stretchingExercises = exercise.filter(x => x.exerciseType === 'stretching');
 
     const cardioExercises = exercise.filter(x => x.exerciseType === 'cardio')
-
-
+    let musclesPerDay = null;
     let exercisesNames = ['chest', 'back', 'shoulders', 'legs', 'triceps', 'biceps']
 
+    HowManyMusclesPerDay(days);
 
     if(days == 2 || days == 3 || days == 6){ // How many days per week athlete can workout
         let workout = []
         for(let i = 1; i <= days; i++){ 
             let dailyExercises = [];
             GetCardioExercise(i, dailyExercises)
-                for(let k = 1; k <= 1; k++){ 
+            
+            
+                for(let k = 1; k <= musclesPerDay; k++){ // how many muscles per day
                     const tempExercise = exercisesNames[Math.floor(Math.random() * exercisesNames.length)] 
                     exercisesNames = exercisesNames.filter(x => x !== tempExercise);
                      if(k == 1) {
                          GetWarmUpExercise(stretchingExercises, dailyExercises);
                      }
-                     HowManyMuclesPerDay(tempExercise, dailyExercises)
+                     HowManyExercisesPerMucle(tempExercise, dailyExercises)
                 }
             workout.push({ day: i, exercises: dailyExercises })
         }
@@ -132,41 +151,87 @@ function GeneratePlan(athlete, exercise){
         return workout;
     }
 
-    function HowManyMuclesPerDay(tempExercise, dailyExercises){
+    function HowManyMusclesPerDay(day){
+        switch(day){
+            case 2:
+                return musclesPerDay = 3;
+            case 3:
+                return musclesPerDay = 2;
+            case 6:
+                return musclesPerDay = 1;
+        }
+    }
+    function HowManyExercisesPerMucle(tempExercise, dailyExercises){
         let isolation = null;
         let compound = null;
-        if(days == 2){
-            if(level === 'beginner'){
-                isolation = 2;
-                compound = 1;
-            } else{
-                isolation = 2;
-                compound = 1;
+        if(gender === 'female'){
+            if(days == 2){
+                if(level === 'beginner'){
+                    isolation = 2;
+                    compound = 1;
+                } else {
+                    isolation = 2;
+                    compound = 1;
+                }
+            } else if(days == 3){
+                if(level === 'beginner'){
+                    isolation = 2;
+                    compound = 1;
+                } else {
+                    isolation = 2;
+                    compound = 1;
+                }
+            } else if(days == 6){
+                if(level === 'beginner'){
+                    isolation = 2;
+                    compound = 2;
+                } else {
+                    isolation = 2;
+                    compound = 4;
+                }
             }
-            
-        } else if(days == 3){
-            if(level === 'beginner'){
-                isolation = 2;
-                compound = 1;
-            } else{
-                isolation = 2;
-                compound = 1;
-            }
-        } else if(days == 6){
-            if(level === 'beginner'){
-                isolation = 2;
-                compound = 2;
-            } else{
-                isolation = 2;
-                compound = 4;
+        } else if(gender === 'male'){
+            if(days == 2){
+                if(level === 'beginner'){
+                    isolation = 2;
+                    compound = 1;
+                } else {
+                    isolation = 2;
+                    compound = 1;
+                }
+            } else if(days == 3){
+                if(level === 'beginner'){
+                    isolation = 2;
+                    compound = 1;
+                } else {
+                    isolation = 2;
+                    compound = 1;
+                }
+            } else if(days == 6){
+                if(level === 'beginner'){
+                    isolation = 2;
+                    compound = 2;
+                } else {
+                    isolation = 2;
+                    compound = 4;
+                }
             }
         }
-
+        
         for(let j = 1; j <= isolation; j++){
-            GetIsolationExercise(tempExercise, isolationExercises, dailyExercises);
+            if(level === 'beginner'){
+                GetIsolationExercise(tempExercise, isolationExercisesForBeginner, dailyExercises);
+            } else {
+                GetIsolationExercise(tempExercise, isolationExercises, dailyExercises);
+            }
+            
         } 
         for(let l = 1; l <= compound; l++){
-            GetCompoundExercise(tempExercise, compoundExercises, dailyExercises);
+            if(level === 'beginner'){
+                GetCompoundExercise(tempExercise, compoundExercisesForBeginner, dailyExercises);
+            } else {
+                GetCompoundExercise(tempExercise, compoundExercises, dailyExercises);
+            }
         }
     }
 
@@ -223,7 +288,12 @@ function GeneratePlan(athlete, exercise){
     }
     
     function GetIsolationExercise(tempExercise, isolationExercises, dailyExercises){
-        let isolation = isolationExercises[tempExercise][Math.floor(Math.random() * isolationExercises[tempExercise].length)]
+        let isolation = null;
+        if(tempExercise === 'legs' && gender === 'female'){
+            isolation = isolationExercises['legsForWomen'][Math.floor(Math.random() * isolationExercises['legsForWomen'].length)]
+        } else {
+            isolation = isolationExercises[tempExercise][Math.floor(Math.random() * isolationExercises[tempExercise].length)]
+        }
         if(level === 'beginner'){
             if(goal === 'lose fat'){
                 sets = 3;
@@ -257,7 +327,12 @@ function GeneratePlan(athlete, exercise){
     }
     
     function GetCompoundExercise(tempExercise, compoundExercises, dailyExercises){
-        let compound = compoundExercises[tempExercise][Math.floor(Math.random() * compoundExercises[tempExercise].length)]
+        let compound = null;
+        if(tempExercise === 'biceps'){
+            compound = isolationExercises['biceps'][Math.floor(Math.random() * isolationExercises['biceps'].length)]
+        } else {
+            compound = compoundExercises[tempExercise][Math.floor(Math.random() * compoundExercises[tempExercise].length)]
+        }
         if(level === 'beginner'){
             if(goal === 'lose fat'){
                 sets = 3;
