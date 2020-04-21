16db4512cd2be18336ddc8cb5eefe46c89d238b6
albisk96 <albertas.kruzintaitis24@gmail.com>
2020-04-21 18:19:36 +0300

    config keys

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const keys = require('./config/keys');
+const keys = require('./keys');
 
 const connectDB = async () => {
 	try {
