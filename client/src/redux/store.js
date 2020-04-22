1c31cb3b58f30f9e30f358a850a491ae8ad4d3fa
albisk96 <albertas.kruzintaitis24@gmail.com>
2020-04-22 09:41:13 +0300

    reader for deploy

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -6,11 +6,10 @@ import thunk from 'redux-thunk';
 import rootReducer from './root-reducer';
 
 const middlewares = [thunk];
-middlewares.push(logger)
 
-// if(process.env.NODE_ENV === 'development'){
-//     middlewares.push(logger)
-// }
+if(process.env.NODE_ENV === 'development'){
+    middlewares.push(logger)
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
