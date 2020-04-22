2520f7b52bfbd29f46821110434e8623633cf7e0
albisk96 <albertas.kruzintaitis24@gmail.com>
2020-04-22 09:14:35 +0300

    added package

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -6,6 +6,7 @@ import thunk from 'redux-thunk';
 import rootReducer from './root-reducer';
 
 const middlewares = [thunk];
+middlewares.push(logger)
 
 // if(process.env.NODE_ENV === 'development'){
 //     middlewares.push(logger)
