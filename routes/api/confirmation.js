c589fdfccfa706798f1dd3e3ff33afebccc88387
albisk96 <albertas.kruzintaitis24@gmail.com>
2020-04-26 18:07:00 +0300

    auth delete

--- routes/api/confirmation.js
+++ routes/api/confirmation.js
@@ -4,7 +4,7 @@ const auth = require('../../middleware/auth');
 
 const User = require('../../models/User');
 
-router.get('/:id', auth, async (req, res) => {
+router.get('/:id', async (req, res) => {
 
     let userId = req.params.id;
 
