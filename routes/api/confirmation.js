932e7728a5dae34239c4da1ef2740b7b829f4c4d
albisk96 <albertas.kruzintaitis24@gmail.com>
2020-04-26 22:04:28 +0300

    console logs

--- routes/api/confirmation.js
+++ routes/api/confirmation.js
@@ -6,21 +6,28 @@ const User = require('../../models/User');
 router.get('/:id', async (req, res) => {
 
     let userId = req.params.id;
+  console.log(`User ID ${userId}`)
 
     let conditions = {
       _id : userId
     }
+
+    console.log()
   
     let update = {
       confirmed: true
     }
 
-    const user = await User.findOneAndUpdate(conditions,update, {
+    const user = await User.findOneAndUpdate(conditions,update, () => console.log('FIND ONE AND UPDATE BLOCK'), {
       new: true
     })
         
       if(user.confirmed){
+        console.log(`User ${user}`)
+        console.log(`User confirmed ${user.confirmed}`)
+        console.log(`env ${NODE.ENV}`)
           return res.redirect('http://tranquil-dawn-70222.herokuapp.com')
+
       } else {
         console.log('error')
       }
