ff9b57871b2f09ddcf93526eeb62852b6da96b50
albisk96 <albertas.kruzintaitis24@gmail.com>
2020-04-27 09:31:05 +0300

    NODE PROD

--- routes/api/confirmation.js
+++ routes/api/confirmation.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const User = require('../../models/User');
 
 router.get('/:id', async (req, res) => {
-
+  console.log('OIFGJREWOIFGMNS______________------------SDFO<SDO:LC<VMO<')
   let userId = req.params.id;
 
   let conditions = {
@@ -20,7 +20,7 @@ router.get('/:id', async (req, res) => {
   
       if(user.confirmed){
         if (process.env.NODE_ENV === 'production'){
-          return res.redirect('http://tranquil-dawn-70222.herokuapp.com')
+          return res.redirect('https://google.com')
           
         } else {
           return res.redirect('http://localhost:3000');
