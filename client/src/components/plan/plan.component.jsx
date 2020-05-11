fc4589f095fa6c5be1685ace6b839ad2db4c8c9d
albisk96 <albertas.kruzintaitis24@gmail.com>
2020-05-11 17:34:39 +0300

    loader in workout plan

--- client/src/components/plan/plan.component.jsx
+++ client/src/components/plan/plan.component.jsx
@@ -4,6 +4,7 @@ import { JumboContainer, PlanContainer, Center } from './plan.styles'
 import { Accordion, Card, Button } from 'react-bootstrap';
 import PlanForm from '../athlete-form/workout-generation.component';
 import PlanTable from './plan-table.component'
+import Spinner from '../spinner/spinner.component';
 
 const Plan = ({ athlete: {athlete} }) => {
 
@@ -19,7 +20,7 @@ const Plan = ({ athlete: {athlete} }) => {
             </div>
             <Accordion>
             {
-                athlete.workoutPlan.map((plan, index) => (
+                athlete.workout !== null ? athlete.workoutPlan.map((plan, index) => (
                 <Card style={{ border: '0px solid transparent' }}>
                 <Card.Header style={{ backgroundColor: '#343a40' }}>
                 <Accordion.Toggle as={Button} style={{ color: 'white' }} variant="link" eventKey={index}>
@@ -37,13 +38,14 @@ const Plan = ({ athlete: {athlete} }) => {
                 </Card.Body>
                 </Accordion.Collapse>
                 </Card>
-                )) 
+                )) : <Spinner />
             }
             </Accordion>
         </PlanContainer>
-        : <PlanContainer>
-                <div>Please, create your Profile first</div>
-                </PlanContainer>
+        : 
+        <PlanContainer>
+            <div>Please, create your Profile first</div>
+        </PlanContainer>
     }
         </Center>
         </div>
