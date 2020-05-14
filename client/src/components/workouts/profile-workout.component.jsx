79111a0f2096bcd5a83f7a21cac5d373257ad4b2
albisk96 <albertas.kruzintaitis24@gmail.com>
2020-05-14 20:07:20 +0300

    workout loader

--- client/src/components/workouts/profile-workout.component.jsx
+++ client/src/components/workouts/profile-workout.component.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import WorkoutCard from '../cards/workout-card.component';
 import { CardColumnsContainer } from './workouts.styles';
+import Spinner from '../../components/spinner/spinner.component';
 
 const WorkoutList = ({ workouts }) => {
+  console.log(workouts)
   return (
     <div className='container'>
-    { workouts ? 
+    { !workouts ?  <Spinner /> : workouts === [] ? <h3>No workouts yet</h3> :
       <div style={{ marginTop: '5%'}}>
         <CardColumnsContainer>
           {workouts.map((workout, index) => (
             <WorkoutCard key={index} workout={workout} />
           ))}
         </CardColumnsContainer>
-        </div> : "This profile do not have any workouts yet"
+        </div> 
       }
       </div>
     )
